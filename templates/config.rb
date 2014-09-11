52be4f437e96c4d85b4540a6c26e0239d3d2d88e
fpizzurro <felice.pizzurro@softecspa.it>
2014-09-11 10:17:22 +0200

    fixed all params in template

--- templates/config.rb
+++ templates/config.rb
@@ -30,15 +30,57 @@
  <% end -%>
 
 <% if scope.lookupvar('backups::enable_nagios') -%>
-  Notifier::Nagios.defaults do |nagios|
-    nagios.on_success   = <%= scope.lookupvar('backups::nagios_success') %>
-    nagios.on_warning   = <%= scope.lookupvar('backups::nagios_warning') %>
-    nagios.on_failure   = <%= scope.lookupvar('backups::nagios_failure') %>
+ Notifier::Nagios.defaults do |nagios|
+   nagios.on_success   = <%= scope.lookupvar('backups::nagios_success') %>
+   nagios.on_warning   = <%= scope.lookupvar('backups::nagios_warning') %>
+   nagios.on_failure   = <%= scope.lookupvar('backups::nagios_failure') %>
 <% if scope.lookupvar('backups::nagios_host') -%>
-    nagios.nagios_host  = '<%= scope.lookupvar('backups::nagios_host') %>'
+   nagios.nagios_host  = '<%= scope.lookupvar('backups::nagios_host') %>'
 <% end -%>
-    nagios.nagios_port  = '<%= scope.lookupvar('backups::nagios_port') %>'
-  end
+   nagios.nagios_port  = '<%= scope.lookupvar('backups::nagios_port') %>'
+ end
+<% end -%>
+
+<% if scope.lookupvar('backups::enable_mail') -%>
+ Notifier::Mail.defaults do |mail|
+   mail.on_success           = <%= scope.lookupvar('backups::mail_success') %>
+   mail.on_warning           = <%= scope.lookupvar('backups::mail_warning') %>
+   mail.on_failure           = <%= scope.lookupvar('backups::mail_failure') %>
+
+   mail.from                 = '<%= scope.lookupvar('backups::mail_from_real') %>'
+   mail.to                   = '<%= scope.lookupvar('backups::mail_to_real') %>'
+   mail.address              = '<%= scope.lookupvar('backups::mail_address') %>'
+   mail.port                 = <%= scope.lookupvar('backups::mail_port') %>
+   mail.domain               = '<%= scope.lookupvar('backups::mail_domain') %>'
+<% if scope.lookupvar('backups::mail_user_name') -%>
+   mail.user_name  = '<%= scope.lookupvar('backups::mail_user_name') %>'
+<% end -%>
+<% if scope.lookupvar('backups::mail_password') -%>
+   mail.password  = '<%= scope.lookupvar('backups::mail_password') %>'
+<% end -%>
+<% if scope.lookupvar('backups::mail_authentication') -%>
+   mail.authentication  = '<%= scope.lookupvar('backups::mail_authentication') %>'
+<% end -%>
+<% if scope.lookupvar('backups::mail_encryption') -%>
+   mail.encryption  = '<%= scope.lookupvar('backups::mail_encryption') %>'
+<% end -%>
+ end
+<% end -%>
+
+<% if scope.lookupvar('backups::enable_hc') -%>
+ Notifier::Hipchat.default do |hipchat|
+   hipchat.on_success = <%= scope.lookupvar('backups::hc_success') %>
+   hipchat.on_warning = <%= scope.lookupvar('backups::hc_warning') %>
+   hipchat.on_failure = <%= scope.lookupvar('backups::hc_failure') %>
+
+   hipchat.success_color = 'green'
+   hipchat.warning_color = 'yellow'
+   hipchat.failure_color = 'red'
+
+   hipchat.token = '<%= scope.lookupvar('backups::hc_token') %>'
+   hipchat.from = '<%= scope.lookupvar('backups::hc_from') %>'
+   hipchat.rooms_notified = '<%= scope.lookupvar('backups::hc_notify') %>'
+ end
 <% end -%>
 
 ##
