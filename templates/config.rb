59da0e68390a3844d0a74443af4ae0f15a186c15
fpizzurro <felice.pizzurro@softecspa.it>
2014-09-11 10:37:30 +0200

    fixed template

--- templates/config.rb
+++ templates/config.rb
@@ -14,72 +14,72 @@
 # Add more (or remove) global configuration below
 
  Backup::Storage::S3.defaults do |s3|
-   s3.access_key_id     = "<%= scope.lookupvar('backups::aws_access_key') %>"
-   s3.secret_access_key = "<%= scope.lookupvar('backups::aws_secret_key') %>"
-   s3.region            = "<%= scope.lookupvar('backups::aws_region') %>"
-   s3.bucket            = "<%= scope.lookupvar('bucket') %>"
+   s3.access_key_id     = "<%= @aws_access_key %>"
+   s3.secret_access_key = "<%= @aws_secret_key %>"
+   s3.region            = "<%= @aws_region %>"
+   s3.bucket            = "<%= @bucket %>"
    s3.keep              = 10
  end
 
- <% if scope.lookupvar('backups::password') != '' -%>
+ <% if @password != '' -%>
  Backup::Encryptor::OpenSSL.defaults do |encryption|
-   encryption.password = "<%= scope.lookupvar('backups::password') %>"
+   encryption.password = "<%= @password %>"
    encryption.base64   = true
    encryption.salt     = true
  end
  <% end -%>
 
-<% if scope.lookupvar('backups::enable_nagios') -%>
+<% if @enable_nagios -%>
  Notifier::Nagios.defaults do |nagios|
-   nagios.on_success   = <%= scope.lookupvar('backups::nagios_success') %>
-   nagios.on_warning   = <%= scope.lookupvar('backups::nagios_warning') %>
-   nagios.on_failure   = <%= scope.lookupvar('backups::nagios_failure') %>
-<% if scope.lookupvar('backups::nagios_host') -%>
-   nagios.nagios_host  = '<%= scope.lookupvar('backups::nagios_host') %>'
+   nagios.on_success   = <%= @nagios_success %>
+   nagios.on_warning   = <%= @nagios_warning %>
+   nagios.on_failure   = <%= @nagios_failure %>
+<% if @nagios_host -%>
+   nagios.nagios_host  = '<%= @nagios_host %>'
 <% end -%>
-   nagios.nagios_port  = '<%= scope.lookupvar('backups::nagios_port') %>'
+   nagios.nagios_port  = '<%= @nagios_port %>'
  end
 <% end -%>
 
-<% if scope.lookupvar('backups::enable_mail') -%>
+<% if @enable_mail -%>
  Notifier::Mail.defaults do |mail|
-   mail.on_success           = <%= scope.lookupvar('backups::mail_success') %>
-   mail.on_warning           = <%= scope.lookupvar('backups::mail_warning') %>
-   mail.on_failure           = <%= scope.lookupvar('backups::mail_failure') %>
+   mail.on_success           = <%= @mail_success %>
+   mail.on_warning           = <%= @mail_warning %>
+   mail.on_failure           = <%= @mail_failure %>
 
-   mail.from                 = '<%= scope.lookupvar('backups::mail_from_real') %>'
-   mail.to                   = '<%= scope.lookupvar('backups::mail_to_real') %>'
-   mail.address              = '<%= scope.lookupvar('backups::mail_address') %>'
-   mail.port                 = <%= scope.lookupvar('backups::mail_port') %>
-   mail.domain               = '<%= scope.lookupvar('backups::mail_domain') %>'
-<% if scope.lookupvar('backups::mail_user_name') -%>
-   mail.user_name  = '<%= scope.lookupvar('backups::mail_user_name') %>'
+   mail.from                 = '<%= @mail_from_real %>'
+   mail.to                   = '<%= @mail_to_real %>'
+   mail.address              = '<%= @mail_address %>'
+   mail.port                 = <%= @mail_port %>
+   mail.domain               = '<%= @mail_domain %>'
+<% if @mail_user_name -%>
+   mail.user_name  = '<%= @mail_user_name %>'
 <% end -%>
-<% if scope.lookupvar('backups::mail_password') -%>
-   mail.password  = '<%= scope.lookupvar('backups::mail_password') %>'
+<% if @mail_password -%>
+   mail.password  = '<%= @mail_password %>'
 <% end -%>
-<% if scope.lookupvar('backups::mail_authentication') -%>
-   mail.authentication  = '<%= scope.lookupvar('backups::mail_authentication') %>'
+<% if @mail_authentication -%>
+   mail.authentication  = '<%= @mail_authentication %>'
 <% end -%>
-<% if scope.lookupvar('backups::mail_encryption') -%>
-   mail.encryption  = '<%= scope.lookupvar('backups::mail_encryption') %>'
+<% if @mail_encryption -%>
+   mail.encryption  = '<%= @mail_encryption %>'
 <% end -%>
  end
 <% end -%>
 
-<% if scope.lookupvar('backups::enable_hc') -%>
+<% if @enable_hc -%>
  Notifier::Hipchat.default do |hipchat|
-   hipchat.on_success = <%= scope.lookupvar('backups::hc_success') %>
-   hipchat.on_warning = <%= scope.lookupvar('backups::hc_warning') %>
-   hipchat.on_failure = <%= scope.lookupvar('backups::hc_failure') %>
+   hipchat.on_success = <%= @hc_success %>
+   hipchat.on_warning = <%= @hc_warning %>
+   hipchat.on_failure = <%= @hc_failure %>
 
    hipchat.success_color = 'green'
    hipchat.warning_color = 'yellow'
    hipchat.failure_color = 'red'
 
-   hipchat.token = '<%= scope.lookupvar('backups::hc_token') %>'
-   hipchat.from = '<%= scope.lookupvar('backups::hc_from') %>'
-   hipchat.rooms_notified = '<%= scope.lookupvar('backups::hc_notify') %>'
+   hipchat.token = '<%= @hc_token %>'
+   hipchat.from = '<%= @hc_from %>'
+   hipchat.rooms_notified = '<%= @hc_notify %>'
  end
 <% end -%>
 
