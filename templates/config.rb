f676941b1e78f2a8abe6102cc007bcb7ba169f04
fpizzurro <felice.pizzurro@softecspa.it>
2014-09-30 12:15:01 +0200

    fix twice backup execution

--- templates/config.rb
+++ templates/config.rb
@@ -100,6 +100,6 @@
 
 ##
 # Load all models from the models directory (after the above global configuration blocks)
-Dir[File.join(File.dirname(Config.config_file), "models", "*.rb")].each do |model|
-  instance_eval(File.read(model))
-end
+#Dir[File.join(File.dirname(Config.config_file), "models", "*.rb")].each do |model|
+#  instance_eval(File.read(model))
+#end
